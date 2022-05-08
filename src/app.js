7a9177d2ba6e8bae1536392b2db9acc9f3027d88
Alexander Leonov <spawnleon@gmail.com>
2022-05-09 00:46:56 +0300

    add keys handler

--- src/app.js
+++ src/app.js
@@ -3,18 +3,84 @@ import './assets/scss/main.scss';
 import keyLines from './assets/js/keys';
 import renderKeyboard from './assets/js/keyboard';
 
+const FLAT_KEY_LINES = keyLines.flat();
+const ENTRY_FIELD = document.querySelector('.entry-field');
+
+const printChar = (char) => {
+  ENTRY_FIELD.value += char;
+};
+
+const pressKeyHandler = (keyData) => {
+  const { key: char, code: keyCode } = keyData;
+  switch (keyCode) {
+    case 'Enter':
+      ENTRY_FIELD.value += '\n';
+      break;
+    case 'Tab':
+      ENTRY_FIELD.value += '\t';
+      break;
+    case 'Delete':
+      ENTRY_FIELD.value = ENTRY_FIELD.value.slice(0, ENTRY_FIELD.selectionStart)
+        + ENTRY_FIELD.value.slice(ENTRY_FIELD.selectionStart + 1);
+      break;
+    case 'Backspace':
+      ENTRY_FIELD.value = ENTRY_FIELD.value.slice(0, ENTRY_FIELD.selectionStart - 1)
+        + ENTRY_FIELD.value.slice(ENTRY_FIELD.selectionStart);
+      break;
+    case 'ShiftLeft':
+    case 'ShiftRight':
+      console.log('shift');
+      break;
+    default:
+      printChar(char);
+  }
+
+  const pressedKey = document.querySelector(`[data-key-code="${keyCode}"]`);
+  pressedKey.classList.add('key--active');
+};
+
+const releaseHandler = (keyData) => {
+  const { key: char, code: keyCode } = keyData;
+  const pressedKey = document.querySelector(`[data-key-code="${keyCode}"]`);
+  pressedKey.classList.remove('key--active');
+};
+
 document.addEventListener('DOMContentLoaded', () => {
   document.querySelector('.keyboard').append(...renderKeyboard(keyLines));
 });
 
-document.addEventListener('keypress', (evt) => {
-  evt.preventDefault();
-  console.log(evt.code);
-  console.log(evt);
-});
-
 document.addEventListener('keydown', (evt) => {
   evt.preventDefault();
-  console.log(evt.code);
-  console.log(evt);
+
+  const { code } = evt;
+  const keyData = FLAT_KEY_LINES.find((k) => k.code === code);
+
+  pressKeyHandler(keyData);
 });
+
+document.addEventListener('keyup', (evt) => {
+  evt.preventDefault();
+  const { code } = evt;
+  const keyData = FLAT_KEY_LINES.find((k) => k.code === code);
+  releaseHandler(keyData);
+});
+
+document.addEventListener('mouseKeyDown', (evt) => {
+  evt.preventDefault();
+  const { detail } = evt;
+  const { code } = detail;
+  const keyData = FLAT_KEY_LINES.find((k) => k.code === code);
+  pressKeyHandler(keyData);
+});
+
+document.addEventListener('mouseKeyUp', (evt) => {
+  evt.preventDefault();
+  const { detail } = evt;
+  const { code } = detail;
+  const keyData = FLAT_KEY_LINES.find((k) => k.code === code);
+  releaseHandler(keyData);
+});
+
+// setInterval(() => {
+//   console.log(ENTRY_FIELD.selectionStart);
+// }, 500);
--- src/assets/js/keyboard.js
+++ src/assets/js/keyboard.js
@@ -2,10 +2,30 @@ const renderKey = (keyData) => {
   const btn = document.createElement('button');
   btn.classList.add('keyboard__key', 'key');
   if (keyData.className) {
-    btn.classList.add('keyboard__key', keyData.className);
+    btn.classList.add(keyData.className);
   }
   btn.type = 'button';
+  btn.dataset.keyCode = keyData.code;
   btn.textContent = keyData.key;
+
+  btn.addEventListener('mousedown', () => {
+    const mouseKeyDown = new CustomEvent('mouseKeyDown', {
+      detail: {
+        code: keyData.code,
+      },
+    });
+    document.dispatchEvent(mouseKeyDown);
+  });
+
+  btn.addEventListener('mouseup', () => {
+    const mouseKeyUp = new CustomEvent('mouseKeyUp', {
+      detail: {
+        code: keyData.code,
+      },
+    });
+    document.dispatchEvent(mouseKeyUp);
+  });
+
   return btn;
 };
 
